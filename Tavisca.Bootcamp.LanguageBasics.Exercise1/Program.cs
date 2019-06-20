857b49c9e11c0fe71e0c9dcc48a2653d96363185
tavisca-schaitanya <schaitanya@tavisca.in>
2019-06-20 17:39:37 +0530

    After First Attempt using operands

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -20,10 +20,38 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"{args} : {result}");
         }
 
+        public static int equateResults(string expectedResult, string actualResult)
+        {
+            System.Console.WriteLine(expectedResult);
+            if(expectedResult.Length != actualResult.Length)
+                return -1;
+            int pos = actualResult.IndexOf('?');
+            return Convert.ToInt32(expectedResult[pos]);    
+        }
+
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            string[] splittedEquation = equation.Split('=');
+            string result = splittedEquation[1];
+            string operand1 = splittedEquation[0].Split('*')[0];
+            string operand2 = splittedEquation[0].Split('*')[1];
+            int missingDigit = -1;
+            if(result.Contains('?'))
+            {
+                int expectedResult = Convert.ToInt32(operand1)*Convert.ToInt32(operand2);
+                missingDigit = equateResults(Convert.ToString(expectedResult), result);
+            }
+            else if(operand1.Contains('?'))
+            {
+                float expectedResult = (float)Convert.ToInt32(result)/Convert.ToInt32(operand2);
+                missingDigit = equateResults(Convert.ToString(expectedResult), operand1);
+            }
+            else if(operand2.Contains('?'))
+            {
+                float expectedResult = (float)Convert.ToInt32(result)/Convert.ToInt32(operand1);
+                missingDigit = equateResults(Convert.ToString(expectedResult), operand2);
+            }
+            return missingDigit; 
         }
     }
 }
